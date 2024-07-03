1805dc1087d209e993e0eac14e514b30ae2be3b5
leejyohan <chyun1529@gmail.com>
2024-07-03 09:22:58 +0900

    test

--- app/user/register/page.tsx
+++ app/user/register/page.tsx
@@ -20,6 +20,7 @@ export default function register() {
 
     function check() {
         console.log(user);
+        
     }
 
     const InputData = (event: React.ChangeEvent<HTMLInputElement>) => {
