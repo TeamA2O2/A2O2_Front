d3b26a680cdac814bfbd4632ac16272c14c1b1b2
vvalvvizal <vvalvvizal@gmail.com>
2024-07-18 00:35:41 +0900

    feat : intro페이지 구현, 로그인 회원가입 라우팅

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
 import Image from "next/image";
-
+import IntroContainer from "@/containers/Intro/IntroContainer";
 export default function Home() {
   return (
     <main>
-      {/* <div>Hello</div> */}
+      <IntroContainer />
     </main>
   );
 }
