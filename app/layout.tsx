e579bb03af41aa0fda9c3f010cc6fdcf4a561918
vvalvvizal <vvalvvizal@gmail.com>
2024-07-27 01:29:21 +0900

    refactor : Link 태그 삭제

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,16 +7,7 @@ const inter = Inter({ subsets: ["latin"] });
 const RootLayout = ({ children }: { children: React.ReactNode }) => (
   <html>
     <body className={inter.className}>
-      <div className="container">
-        {/* <nav>
-          <Link href="/user/info">userInfo /</Link>
-          <Link href="/user/register">register /</Link>
-          <Link href="/user/login">login /</Link>
-          <Link href="/funding/create">create /</Link>
-          <Link href="/funding/detail/41">detail(41)</Link>
-        </nav> */}
-        {children}
-      </div>
+      <div className="container">{children}</div>
     </body>
   </html>
 );
